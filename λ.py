43e9bef3e67f87d9fdd62f8e00bc2416a1418f6a
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-19 21:13:43 -0700

    started parser

--- λ.py
+++ λ.py
@@ -1,22 +1,34 @@
-from string import ascii_lowercase, ascii_uppercase
-
 data = ""
-with open("test.λ", "r") as f:
+with open("programs/nums.λ", "r") as f:
     data = f.read().split("\n")
 
+# lexer
 toks = []
 for d in data:
     tok = []
     cur = 0
     while cur < len(d):
-        if d[cur] in ascii_lowercase + "λ.=\n":
+        if d[cur] in "λ.=\n" or 97 <= ord(d[cur]) <= 122 or 40 <= ord(d[cur]) <= 41:
             tok.append(d[cur])
-        if d[cur] in ascii_uppercase:
+        if 65 <= ord(d[cur]) <= 90:
             var = ""
-            while cur < len(d) and d[cur] in ascii_uppercase:
+            while cur < len(d) and (65 <= ord(d[cur]) <= 90 or 48 <= ord(d[cur]) <= 57):
                 var += d[cur]
                 cur += 1
             tok.append(var)
         cur += 1
     toks.append(tok)
 print(toks)
+
+dct = {}
+for i in toks:
+    dct[i[0]] = list(map(lambda x : dct[x] if 65 <= ord(x[0]) <= 90 or 48 <= ord(x) <= 57 else x, i[2:]))
+print(dct["OUT"])
+
+def create_ast(x):
+    if x[0] == "λ":
+        return [x[1], create_ast(x[3:])]
+    if x[0] == "(":
+        print(x)
+        return [create_ast(x[:x.index(")")]), x[x.index(")")+1:]]
+print(create_ast(["(", ")"]))
